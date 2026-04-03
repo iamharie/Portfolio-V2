0d4bd9be0a80beb91fbb7f72423a3dfa123bdb63
Hariharan Mohan <haranhari350@gmail.com>
2026-04-03 00:24:59 -0400

    Feat: Icon tile fix

--- src/components/models/workExperience.ts
+++ src/components/models/workExperience.ts
@@ -1,36 +1,36 @@
 export const jobDetails = [
   {
-    title: "Verizon 🇺🇸",
+    title: "Surge Technology Solutions (Client: Verizon)",
     role: "Software Developer",
-    description: "Aug 2024 - Present 🟢",
+    description: "Aug 2024 - Present",
     icon: "🏢",
   },
   {
-    title: "Porter Lee Corporation 🇺🇸",
+    title: "Porter Lee Corporation",
     role: "Software Developer Intern",
     description: "May 2024 - Jul 2024",
     icon: "🏢",
   },
   {
-    title: "Wichita State University 🇺🇸",
+    title: "Wichita State University",
     role: "Graduate Research Assistant",
     description: "Jun 2023 - May 2024",
     icon: "👨🏻‍🎓",
   },
   {
-    title: "Wichita State University 🇺🇸",
+    title: "Wichita State University",
     role: "Student Technician",
     description: "Jan 2023 - May 2023",
     icon: "👨🏻‍🎓",
   },
   {
-    title: "Surge Technology Solutions 🇮🇳",
+    title: "Surge IT Technology Solutions",
     role: "Software Engineer",
     description: "Mar 2021 - Jun 2022",
     icon: "🏢",
   },
   {
-    title: "Accenture 🇮🇳",
+    title: "Accenture",
     role: "Full Stack Engineer",
     description: "Apr 2019 - Mar 2021",
     icon: "🏢",
